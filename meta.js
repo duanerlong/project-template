6fef97e04a7f3a0d4c4ec5da4ec26fc834600816
duan <duan@azest.co.jp>
2021-02-04 23:29:19 +0900

    init

---
+++ meta.js
@@ -0,0 +1,34 @@
+/**
+ * Meta JS File that will be picked up by the Vue CLI
+ */
+module.exports = {
+    /**
+     * Add your own Prompt questions here!
+     */
+    prompts: {
+      name: {
+        type: 'string', // Question type
+        required: true, // Is question Required
+        message: 'Project name' // Question it's self
+      },
+      description: {
+        type: 'string',
+        required: false,
+        message: '',
+        default: 'My Awesome Project'
+      },
+      author: {
+        type: 'string',
+        message: 'Author'
+      },
+      extra: {
+        type: 'string',
+        message: 'Extra Messaging!'
+      }
+    },
+  
+    /**
+     * You can add a custom complete message
+     */
+    completeMessage: 'Project Complete!'
+  };

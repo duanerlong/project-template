6f65ba03bdc8af27fa22b6c96cd657b9fe69f014
duan <duan@azest.co.jp>
2021-02-12 11:35:38 +0900

    test

---
+++ template/components/app-background.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-0">
+    <img :src="src" :srcset="srcset" class="w-100 fixed-top" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  height: 100vh;
+  width: 100vw;
+  z-index: -1 !important;
+  object-fit: cover;
+  // opacity: 50%;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    srcset: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
